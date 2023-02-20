7ded5a1ace436be68fc932bceed6535544d8968b
David Eck <eck@hws.edu>
2023-02-20 11:33:14 -0500

    Added a simulation of xComputer in Logisim

--- xComputer/tmcm-java-source/tmcm/xTurtle/TurtleNotification.java
+++
@@ -1,9 +0,0 @@
-
-package tmcm.xTurtle;
-
-interface TurtleNotification {
-   public void errorReport(String errorMessage, int position);
-   public void startRunning(TProcess runner);
-   public void doneRunning();
-}
-
